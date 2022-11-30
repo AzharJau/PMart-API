c8ae29fce002c32802ed5b50b40f2a5d21485000
AzharJau <jauharulumamazhar@gmail.com>
2022-11-30 14:25:06 +0700

    push

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stripeRoute = require("./routes/stripe")
 const ordersRoute = require("./routes/order")
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://padjadjaran-mart.netlify.app/', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
